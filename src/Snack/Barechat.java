159951d35687d32184dc2b6fda5f797266d61271
Gbolhan-sudo <gbolahanadegunwa@gmail.com>
2023-05-05 09:50:46 +0100

    my commit

---
+++ src/Snack/Barechat.java
@@ -0,0 +1,40 @@
+package Snack;
+
+import java.util.Scanner;
+
+public class Barechat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter second number");
+        int secondNumber = input.nextInt();
+        System.out.println("Enter third number");
+        int thirdNumber = input.nextInt();
+        System.out.println("Enter forth number");
+        int forthNumber = input.nextInt();
+        System.out.println("Enter fifth number");
+        int fiftyNumber = input.nextInt();
+    }
+    public void firstNumber(){
+        int i = 0;
+        System.out.println(i == 30);
+    }
+
+//    public static void firstNumber(int firstNum){
+//        int count = 1;
+//        int firstNumber = 0;;
+//        while (count <= firstNum){
+//            if (firstNum >= count =0){
+//                firstNumber++;
+//            }
+//            count++;
+//        }
+//        if (count == firstNumber){
+//            System.out.println(firstNumber + "secondNumber");
+//        }
+//        else {
+//            System.out.println(firstNumber + "Enter first number again");
+//        }
+//    }
+}
