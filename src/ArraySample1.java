7d761e5655cd9a23ded27691fafd454fe982d6d6
Gbolhan-sudo <gbolahanadegunwa@gmail.com>
2023-05-05 10:10:58 +0100

    my commit

---
+++ src/ArraySample1.java
@@ -0,0 +1,18 @@
+public class ArraySample1 {
+    public static void main(String[] args) {
+//        create array
+        int[] array = new int[10];//[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        System.out.println(array);
+//insert element into array
+        array[2] = 20;//[0,0,20,0,0,0,0,0,0,0,0]
+        System.out.println(array);
+    }
+
+
+    private String toString(int[] array) {
+        String arrayRepresentation;
+        for (int index = 0; index < array.length; index++) {
+
+        }
+    }
+}
