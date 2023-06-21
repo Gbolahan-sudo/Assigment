27e1f4706f5d388acd02b454cfae4f2b42f07fbb
Gbolhan-sudo <gbolahanadegunwa@gmail.com>
2023-06-21 18:17:46 +0100

    my commit

--- src/UbaBank/Bank.java
+++ src/UbaBank/Bank.java
@@ -12,17 +12,21 @@ public class Bank {
     public void registerCustomer(String registerCustomer) {
         this.registerCustomer = registerCustomer;
     }
-    public String generateAccountNumber(String phoneNumber){
+
+    public String generateAccountNumber(String phoneNumber) {
         String accountNumber = phoneNumber.substring(1);
         return accountNumber;
     }
-    public int sizeAccount(){
+
+    public int sizeAccount() {
         return accounts.size();
     }
+
     public String RegisterCustomer() {
         return registerCustomer;
     }
-    public void deposit(String accountNumber,int amount) {
+
+    public void deposit(String accountNumber, int amount) {
         this.balance += amount;
     }
 
@@ -32,12 +36,21 @@ public class Bank {
 
     public void withdraw(String accountNumber, int amount, String pin) {
         this.balance -= amount;
-        if (balance > 0 && balance <  amount) {
+        if (balance > 0 && balance < amount) {
 
-        }else {
+        } else {
             System.out.println("Insufficient balance");
 
         }
     }
-}
 
+
+    public void transfer(String accountNumber, String receiverAccount,int amount, String pin) {
+        this.balance -= amount;
+        if (balance == amount){
+            System.out.println("Transaction successful");
+        }else{
+            System.out.println("Wrong transaction");
+        }
+    }
+}
